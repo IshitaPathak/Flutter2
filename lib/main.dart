07fa0ad45a4cfb962c8d08c94852a48f58f2c343
IshitaPathak <pathakishita111@gmail.com>
2023-02-18 14:07:52 +0530

    storing data

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:bhu_app/pages/home.dart';
 import 'package:bhu_app/pages/about.dart';
 import 'package:bhu_app/pages/contact.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: '/home',
-    routes: {
-      '/home': (context) => Home(),
-      '/about': (context) => AboutUs(),
-      '/contact': (context) => ContactUs(),
-    },
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => Home(),
+        '/about': (context) => AboutUs(),
+        '/contact': (context) => ContactUs(),
+      },
+    );
+  }
 }
--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -1,50 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ContactUs extends StatelessWidget {
-  const ContactUs({super.key});
+  TextEditingController name = new TextEditingController();
+  TextEditingController email = new TextEditingController();
+  TextEditingController message = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('BHU-APP'),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter you name',
-                ),
+      appBar: AppBar(
+        title: Text('BHU-APP'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller: name,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter your name',
               ),
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter you email',
-                ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller: email,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter your email',
               ),
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter you message',
-                ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller: message,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter your message',
               ),
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-              child:
-                  ElevatedButton(onPressed: () {}, child: const Text('SUBMIT')),
-            )
-          ],
-        ));
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: ElevatedButton(
+              onPressed: () {
+                Map<String, dynamic> data = {
+                  'field1': name.text,
+                  'field2': email.text,
+                  'field3': message.text,
+                };
+                FirebaseFirestore.instance.collection("Users").add(data);
+              },
+              child: const Text('SUBMIT'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
