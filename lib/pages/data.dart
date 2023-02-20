f15b3a2ed0689821f61d7af4300b78b45eb737df
IshitaPathak <pathakishita111@gmail.com>
2023-02-20 14:55:39 +0530

    fetching data

--- lib/pages/data.dart
+++ lib/pages/data.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:bhu_app/pages/contact.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:bhu_app/pages/contact.dart';
 
-CollectionReference userDataCollection =
-    FirebaseFirestore.instance.collection('Users');
-late Stream<QuerySnapshot> dataItems;
 // two main ways to get the list of documents
 // 1. call get() collection reference, this will gives a Future (we can't listen to Future)
 // 2. by calling function snapshots() on collection reference, and this returns stream
 
 class MyData extends StatefulWidget {
-  const MyData({super.key});
+  const MyData({Key? key}) : super(key: key);
 
   @override
   State<MyData> createState() => _MyDataState();
 }
 
 class _MyDataState extends State<MyData> {
-  initState() {
+  late Stream<QuerySnapshot> dataItems;
+
+  @override
+  void initState() {
     super.initState();
-    dataItems = userDataCollection.snapshots();
+    dataItems = FirebaseFirestore.instance.collection('Users').snapshots();
   }
 
   @override
@@ -29,17 +29,27 @@ class _MyDataState extends State<MyData> {
         title: Text('Data'),
       ),
       body: StreamBuilder<QuerySnapshot>(
-          stream: dataItems,
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasError) {
-              return Text(snapshot.error.toString());
-            }
-            if (snapshot.connectionState == ConnectionState.active) {
-              QuerySnapshot? querySnapshot = snapshot.data;
-            }
+        stream: dataItems,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
             return Center(child: CircularProgressIndicator());
-          }),
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              DocumentSnapshot userData = snapshot.data!.docs[index];
+              return ListTile(
+                  title: Column(
+                children: [
+                  Text(userData['field1']),
+                  Text(userData['field2']),
+                  Text(userData['field3'])
+                ],
+              ));
+            },
+          );
+        },
+      ),
     );
   }
 }
